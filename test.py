43911ab5cb9a7807d7ea487d0647899372ec5b43
Pal <105495138+pal404blog@users.noreply.github.com>
2023-12-28 12:02:09 +0530

    2023 5th sem

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ chrome_options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome('chromedriver',chrome_options=chrome_options)
 wb = openpyxl.Workbook()
 ws = wb.active
-data = (("ID","Name","CGPA", "SGPA", " 	DATA STRUCTURES & ALGORITHMS(Theory)","DATA STRUCTURES & ALGORITHMS(Practical)", " 	 	DATABASE MANAGEMENT SYSTEM(Theory)", "  	DATABASE MANAGEMENT SYSTEM(Practical)","SOFTWARE GROUP PROJECT-II", "MPCO(Theory)","MPCO(Practical)", "Python", "Maintanence", "HS"))
+data = (("ID","Name","CGPA", "SGPA"))
 ws.append(data)
 for x in range(181):
     driver.get('https://charusat.edu.in:912/UniExamResult/frmUniversityResult.aspx') # Open Quora website
@@ -28,11 +28,11 @@ for x in range(181):
 
     a3 = driver.find_element('xpath','//*[@id="ddlSem"]') # HTML tag element for password field
     drop = Select(a3)
-    drop.select_by_visible_text('4')
+    drop.select_by_visible_text('5')
 
     a4 = driver.find_element('xpath','//*[@id="ddlScheduleExam"]') # HTML tag element for password field
     drop = Select(a4)
-    drop.select_by_visible_text('APRIL 2023')
+    drop.select_by_visible_text('NOVEMBER 2023')
 
     a5 = driver.find_element('xpath','//*[@id="txtEnrNo"]') # HTML tag element for password field
     
@@ -60,31 +60,31 @@ for x in range(181):
         b1 = driver.find_element('xpath','/html/body/form/div[3]/div/div/table[3]/tbody/tr[1]/td[1]')
         name = driver.find_element('xpath','//*[@id="uclGrd1_lblStudentName"]')
         idno = driver.find_element('xpath','//*[@id="uclGrd1_lblExamNo"]')
-        b3 = driver.find_element('xpath','/html/body/form/div[3]/div/div/table[4]/tbody/tr[1]/td[4]')
-        b4 = driver.find_element('xpath','//*[@id="uclGrd1_lblSGPA"]')
-        b5 = driver.find_element('xpath','/html/body/form/div[3]/div/div/table[3]/tbody/tr[1]/td[4]')
-        b6 = driver.find_element('xpath','//*[@id="uclGrd1_lblExamNo"]')
-        b7 = driver.find_element('xpath', '//*[@id="uclGrd1_grdResult_ctl02_lblSubjectName"]')
-        sgp = driver.find_element('xpath', '//*[@id="uclGrd1_grdResult_ctl02_lblGrade"]')
-        javat = driver.find_element('xpath', '//*[@id="uclGrd1_grdResult_ctl03_lblGrade"]')
-        javap = driver.find_element('xpath', '//*[@id="uclGrd1_grdResult_ctl04_lblGrade"]')
-        det = driver.find_element('xpath', '//*[@id="uclGrd1_grdResult_ctl05_lblGrade"]')
-        dep = driver.find_element('xpath', '//*[@id="uclGrd1_grdResult_ctl06_lblGrade"]')
-        dcnt = driver.find_element('xpath', '//*[@id="uclGrd1_grdResult_ctl07_lblGrade"]')
-        dcnp= driver.find_element('xpath', '//*[@id="uclGrd1_grdResult_ctl08_lblGrade"]')
-        art = driver.find_element('xpath', '//*[@id="uclGrd1_grdResult_ctl09_lblGrade"]')
-        creative = driver.find_element('xpath', '//*[@id="uclGrd1_grdResult_ctl10_lblGrade"]')
-        math = driver.find_element('xpath', '//*[@id="uclGrd1_grdResult_ctl11_lblGrade"]')
-        cgpa = driver.find_element('xpath', '//*[@id="uclGrd1_lblCGPA"]')
-        sgpa = driver.find_element('xpath', '//*[@id="uclGrd1_lblSGPA"]')
+        # b3 = driver.find_element('xpath','/html/body/form/div[3]/div/div/table[4]/tbody/tr[1]/td[4]')
+        # b4 = driver.find_element('xpath','//*[@id="uclGrd1_lblSGPA"]')
+        # b5 = driver.find_element('xpath','/html/body/form/div[3]/div/div/table[3]/tbody/tr[1]/td[4]')
+        # b6 = driver.find_element('xpath','//*[@id="uclGrd1_lblExamNo"]')
+        # b7 = driver.find_element('xpath', '//*[@id="uclGrd1_grdResult_ctl02_lblSubjectName"]')
+        # sgp = driver.find_element('xpath', '//*[@id="uclGrd1_grdResult_ctl02_lblGrade"]')
+        # javat = driver.find_element('xpath', '//*[@id="uclGrd1_grdResult_ctl03_lblGrade"]')
+        # javap = driver.find_element('xpath', '//*[@id="uclGrd1_grdResult_ctl04_lblGrade"]')
+        # det = driver.find_element('xpath', '//*[@id="uclGrd1_grdResult_ctl05_lblGrade"]')
+        # dep = driver.find_element('xpath', '//*[@id="uclGrd1_grdResult_ctl06_lblGrade"]')
+        # dcnt = driver.find_element('xpath', '//*[@id="uclGrd1_grdResult_ctl07_lblGrade"]')
+        # dcnp= driver.find_element('xpath', '//*[@id="uclGrd1_grdResult_ctl08_lblGrade"]')
+        # art = driver.find_element('xpath', '//*[@id="uclGrd1_grdResult_ctl09_lblGrade"]')
+        # creative = driver.find_element('xpath', '//*[@id="uclGrd1_grdResult_ctl10_lblGrade"]')
+        # math = driver.find_element('xpath', '//*[@id="uclGrd1_grdResult_ctl11_lblGrade"]')
+        cgpa = driver.find_element('xpath', '//*[@id="uclGrd1_tblCumulative"]/tbody/tr[2]/td[3]')
+        sgpa = driver.find_element('xpath', '//*[@id="form1"]/div[3]/div/div/table[4]/tbody/tr[2]/td[3]')
 
 
 
 
 
-        data1 = ((idno.text,name.text,cgpa.text , sgpa.text, sgp.text,javat.text,javap.text,det.text,dep.text,dcnt.text,dcnp.text, art.text,creative.text, math.text))
+        data1 = ((idno.text,name.text,cgpa.text , sgpa.text))
         ws.append(data1)
-        wb.save("result2023.xlsx")
+        wb.save("result2024.xlsx")
         # f = open("results.txt", "a")
         # f.write(b1.text + ":" + name.text + "\n" + b5.text + ":" + b6.text + "\n" + b3.text + ":" + b4.text +"\n \n")
         # f.close()
