3245ad0367c18d13079c5f798a13ff5532208120
Pal <105495138+pal404blog@users.noreply.github.com>
2023-12-28 12:41:11 +0530

    Update test.py

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome()
+driver = webdriver.Firefox()
 
 wb = openpyxl.Workbook()
 ws = wb.active
