c0cbb8215359f8c9a888d1b55122fa907d794ab0
Pal <105495138+pal404blog@users.noreply.github.com>
2023-12-28 12:56:59 +0530

    Update test.py

--- test.py
+++ test.py
@@ -8,11 +8,6 @@ from selenium import webdriver
 import openpyxl
 
 
-
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome()
 
 wb = openpyxl.Workbook()
